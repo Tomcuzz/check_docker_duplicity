9014a98e33f0ae4315186f00b5c297201acd63f1
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2022-11-14 11:00:48 +0000

    Install dependencies on setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+apt-get install python3-pip
+pip3 install -U future 
+
 cp ./check_docker_duplicity /usr/lib/nagios/plugins/check_docker_duplicity
 chmod 755 /usr/lib/nagios/plugins/check_docker_duplicity
